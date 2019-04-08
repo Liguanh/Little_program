74061727059c6b1eeac5e9fd6f8a2a62a6c8a161
linguanghui <lin.guanghui@sunfund.com>
2019-04-08 23:55:25 +0800

    databases

--- database/migrations/2019_03_29_112642_create_jy_user_bonus_table.php
+++ database/migrations/2019_03_29_112642_create_jy_user_bonus_table.php
@@ -17,8 +17,8 @@ class CreateJyUserBonusTable extends Migration
             $table->increments('id')->comment('红包发放记录表');
             $table->integer('user_id')->default(0)->comment('用户id');
             $table->integer('bonus_id')->default(0)->comment('红包id');
-            $table->timestamp('start_time')->comment('开始时间');
-            $table->timestamp('end_time')->comment('结束时间');
+            $table->datetime('start_time')->comment('开始时间');
+            $table->datetime('end_time')->comment('结束时间');
             $table->enum('status',[1,2,3])->default('1')->comment('1未使用 2 已使用 3 已过期');
             $table->timestamps();
         });
