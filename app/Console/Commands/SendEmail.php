754ebe71c657ffa1e0dd53a865c1375bf3a9abe7
Liguanh <lgh189491@163.com>
2019-04-17 15:56:51 +0800

    定时发送邮件

--- app/Console/Commands/SendEmail.php
+++ app/Console/Commands/SendEmail.php
@@ -40,12 +40,23 @@ class SendEmail extends Command
     {
         //
 
+        $m_nums = \DB::table('jy_user')->count();
+        $order_nums = \DB::table('jy_order')->count();
+        $goods_nums =\DB::table('jy_goods')->count();
+
+        $viewData = [
+            'url' => 'email.data',
+            'assign' => [
+                'm_nums' => $m_nums,
+                'order_nums' => $order_nums,
+                'goods_nums' => $goods_nums,
+            ],
+        ];
         $emailData = [
-            'subject' => '测试发邮件',
-            'content' => "测试laravel的任务调度控制台",
+            'subject' => date("Y-m-d").'_数据统计邮件',
             'email_address' => '872947247@qq.com'
         ];
 
-        ToolsEmail::sendEmail($emailData);
+        ToolsEmail::sendHtmlEmail($viewData,$emailData);
     }
 }
---
+++ resources/views/email/data.blade.php
@@ -0,0 +1,21 @@
+<div> <h3>邮件统计信息</h3></div>
+
+<p>
+	
+	<table border="1" cellspacing="0">
+		<thead>
+			<tr>
+			<th>用户注册数</th>
+			<th>订单总数</th>
+			<th>商品总数</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>{{$m_nums}}</td>
+				<td>{{$order_nums}}</td>
+				<td>{{$goods_nums}}</td>
+			</tr>
+		</tbody>
+	</table>
+</p>
