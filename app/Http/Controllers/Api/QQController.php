8f595229b9603bdfc653d77ff6c57ce21d93d8a4
Liguanh <lgh189491@163.com>
2019-05-05 14:16:54 +0800

    第三方登陆

--- app/Http/Controllers/Api/QQController.php
+++ app/Http/Controllers/Api/QQController.php
@@ -7,11 +7,34 @@ use App\Http\Controllers\Controller;
 
 class QQController extends Controller
 {
+	protected $qq = null;
+
+	public function __construct()
+	{
+		$this->qq = \Config::get('qq');
+
+	}
+
     //QQ地址回调
     public function callback(Request $request)
     {
     	$params = $request->all();
+    	\Log::info('Step1: QQ第三方登陆获取code授权码:',[$params]);
 
-    	\Log::info('QQ第三方登陆地址回调',[$params]);
+    	//Step1:  点击链接地址获取授权码
+    	if(isset($params['code'])){//code授权码存在
+
+    		$code = $params['code'];//code授权码
+
+    		//获取token的url地址信息
+    		$tokenUrl = sprintf($this->qq['token_url'],$this->qq['app_id'],$this->qq['app_key'],$code,$this->qq['redirect_url']);
+
+    		\Log::info('QQ第三方登陆获取access_token的url地址', ['access_token_url'=>$tokenUrl]);
+
+    		$response = file_get_contents($tokenUrl);
+
+    		\Log::info('请求access_token返回的内容',[$response]);
+
+    	}
     }
 }
---
+++ config/qq.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'app_id' => '101572231',
+	'app_key' => '59ff09ec4da3c5b160638918b9700ee4',
+	'redirect_url' => 'http://www.shopyjr.com/qq/callback',
+	//获取access_token的url地址
+	'token_url' => 'https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=%s&client_secret=%s&code=%s&redirect_uri=%s',
+
+];
