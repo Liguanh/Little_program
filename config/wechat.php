a3c2b9b19bde0f76b6a1ae6e2e092456c470fa21
Liguanh <lgh189491@163.com>
2019-05-06 19:32:26 +0800

    test

--- config/wechat.php
+++ config/wechat.php
@@ -2,9 +2,9 @@
 //配置微信公众号的信息
 
 return [
-	'app_id' => "wxce9efeca70c08b5b",//开发者的id
-	'app_secret' => "4215d057b7f827f95786958b8ad31b16",//开发者的秘钥
-	'token' => "lghweixin",//token值
+	'app_id' => "wxf2b8313e84f65edf",//开发者的id
+	'app_secret' => "bfd7c72a67af9983830a3323a2d668f5",//开发者的秘钥
+	'token' => "9kI3GSayOOgVAsafnLdBpYJ3B5elyYyX",//token值
 	'access_token_url' => 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s',//获取access_token的地址
 
 	'menu_url' => 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s',//获取自定义菜单接口url地址
