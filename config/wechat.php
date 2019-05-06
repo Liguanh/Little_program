8c94c67af67f7001135d3228e6f4fe12548812c9
Liguanh <lgh189491@163.com>
2019-05-06 10:30:40 +0800

    微信公众号二次开发

--- config/wechat.php
+++ config/wechat.php
@@ -2,7 +2,7 @@
 //配置微信公众号的信息
 
 return [
-	'app_id' => "",//开发者的id
-	'app_secret' => "",//开发者的秘钥
+	'app_id' => "wxce9efeca70c08b5b",//开发者的id
+	'app_secret' => "4215d057b7f827f95786958b8ad31b16",//开发者的秘钥
 	'token' => "lghweixin",//token值
 ];
