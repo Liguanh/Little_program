bab1a24b66a503038ecd618ea94af35bbb2f0480
Liguanh <lgh189491@163.com>
2019-05-08 10:18:25 +0800

    test

--- resources/views/wap/code.blade.php
+++ resources/views/wap/code.blade.php
@@ -1,1 +1,10 @@
-<a href="{{$h5_url}}">授权</a>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>一点网站,网页授权</title>
+</head>
+<body>
+
+	<center><a href="{{$h5_url}}"><h4>授权</h4></a></center>
+</body>
+</html>
