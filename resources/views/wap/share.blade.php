1dacd2f88c62614dafefddceedc6b102e36326a7
Liguanh <lgh189491@163.com>
2019-05-08 16:16:35 +0800

    test

--- resources/views/wap/share.blade.php
+++ resources/views/wap/share.blade.php
@@ -31,7 +31,7 @@
 		    timestamp: '{{$timestamp}}', // 必填，生成签名的时间戳
 		    nonceStr: '{{$nonceStr}}', // 必填，生成签名的随机串
 		    signature: '{{$signature}}',// 必填，签名
-		    jsApiList: ['checkJsApi','onMenuShareAppMessage','onMenuShareTimeline'] // 必填，需要使用的JS接口列表
+		    jsApiList: ['checkJsApi','onMenuShareAppMessage','onMenuShareTimeline','updateAppMessageShareData'] // 必填，需要使用的JS接口列表
 		});
 		wx.ready(function(){
 			// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
@@ -69,7 +69,15 @@
 		});
 		//分享给朋友
 		function shareFriends(){
-			
+			wx.updateAppMessageShareData({ 
+		       	title: '测试分享', // 分享标题
+		        desc: '测试分享的内容', // 分享描述
+		        link: 'http://www.shopyjr.com/api/wap/share', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		        imgUrl: 'http://www.shopyjr.com/images/photos/blog4.jpg', // 分享图标
+		        success: function () {
+		          // 设置成功
+		        }
+		    })
 		}
 		
 	</script>
